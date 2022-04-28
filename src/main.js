99c3bc70dced7cd0d929975a75a08cbea3f8e4f4
takeaways <wkdrjsdlf2@gmail.com>
2022-04-29 00:18:37 +0900

    feat: kind of camera

--- src/main.js
+++ src/main.js
@@ -17,15 +17,33 @@ renderer.setSize(
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
+
+// 원근이 보이는 카메라
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1, //near
   1000 //far
 );
+
+// 원근 없이 같이 보이는 카메라
+// const camera = new THREE.OrthographicCamera(
+//   -(window.innerWidth / window.innerHeight), //
+//   window.innerWidth / window.innerHeight,
+//   1,
+//   -1,
+//   0.1,
+//   1000
+// );
+
 camera.position.x = 1;
 camera.position.y = 2;
 camera.position.z = 5;
+
+camera.lookAt(0, 0, 0);
+camera.zoom = 0.5;
+camera.updateProjectionMatrix();
+
 scene.add(camera);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
