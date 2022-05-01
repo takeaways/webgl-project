a2c63b69719da009af7feecf6a87c461dcaf3eca
takeaways <wkdrjsdlf2@gmail.com>
2022-05-01 14:34:36 +0900

    feat: check raycaster beam

--- src/ex01.js
+++ src/ex01.js
@@ -1,69 +1,101 @@
-import * as THREE from 'three';
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-// ----- 주제: 
+// ----- 주제:
 
 export default function example() {
-	// Renderer
-	const canvas = document.querySelector('#three-canvas');
-	const renderer = new THREE.WebGLRenderer({
-		canvas,
-		antialias: true
-	});
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+  // Renderer
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
 
-	// Scene
-	const scene = new THREE.Scene();
+  // Scene
+  const scene = new THREE.Scene();
 
-	// Camera
-	const camera = new THREE.PerspectiveCamera(
-		75,
-		window.innerWidth / window.innerHeight,
-		0.1,
-		1000
-	);
-	camera.position.y = 1.5;
-	camera.position.z = 4;
-	scene.add(camera);
+  // Camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.x = 1;
+  camera.position.y = 1.5;
+  camera.position.z = 4;
+  scene.add(camera);
 
-	// Light
-	const ambientLight = new THREE.AmbientLight('white', 0.5);
-	scene.add(ambientLight);
+  // Light
+  const ambientLight = new THREE.AmbientLight("white", 0.5);
+  scene.add(ambientLight);
 
-	const directionalLight = new THREE.DirectionalLight('white', 1);
-	directionalLight.position.x = 1;
-	directionalLight.position.z = 2;
-	scene.add(directionalLight);
+  const directionalLight = new THREE.DirectionalLight("white", 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
 
-	// Controls
+  // Controls
+  const orbitControl = new OrbitControls(camera, renderer.domElement);
 
-	// Mesh
-	const geometry = new THREE.BoxGeometry(1, 1, 1);
-	const material = new THREE.MeshStandardMaterial({
-		color: 'seagreen'
-	});
-	const mesh = new THREE.Mesh(geometry, material);
-	scene.add(mesh);
+  // Mesh
+  const lineMeterial = new THREE.LineBasicMaterial({ color: "yello" });
+  const points = [];
+  points.push(new THREE.Vector3(0, 0, 100));
+  points.push(new THREE.Vector3(0, 0, -100));
+  const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
+  const guide = new THREE.Line(lineGeometry, lineMeterial);
+  scene.add(guide);
 
-	// 그리기
-	const clock = new THREE.Clock();
+  //
+  const raycaster = new THREE.Raycaster();
 
-	function draw() {
-		const delta = clock.getDelta();
+  const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+  const boxMaterial = new THREE.MeshStandardMaterial({ color: "pulm" });
+  const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+  boxMesh.name = "box";
+  const torusGeometry = new THREE.TorusGeometry(2, 0.5, 16, 100);
+  const torusMaterial = new THREE.MeshStandardMaterial({ color: "lime" });
+  const torusMesh = new THREE.Mesh(torusGeometry, torusMaterial);
+  torusMesh.name = "torus";
+  scene.add(boxMesh, torusMesh);
+  const meshes = [boxMesh, torusMesh];
+  // 그리기
+  const clock = new THREE.Clock();
 
-		renderer.render(scene, camera);
-		renderer.setAnimationLoop(draw);
-	}
+  function draw() {
+    const delta = clock.getDelta();
 
-	function setSize() {
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
-		renderer.setSize(window.innerWidth, window.innerHeight);
-		renderer.render(scene, camera);
-	}
+    const time = clock.getElapsedTime();
 
-	// 이벤트
-	window.addEventListener('resize', setSize);
+    boxMesh.position.y = Math.sin(time) * 2;
+    torusMesh.position.y = Math.cos(time) * 2;
+    boxMesh.material.color.set("plum");
+    torusMesh.material.color.set("lime");
 
-	draw();
+    const origin = new THREE.Vector3(0, 0, 100);
+    const direction = new THREE.Vector3(0, 0, -1);
+    raycaster.set(origin, direction);
+    const intersects = raycaster.intersectObjects(meshes);
+    intersects.forEach((item) => {
+      item.object.material.color.set("red");
+    });
+
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener("resize", setSize);
+
+  draw();
 }
